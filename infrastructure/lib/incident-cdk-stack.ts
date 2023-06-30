0a28c6f84d5d7110a7ba9d0df4101edcbd0e6537
Will <will86325@gmail.com>
2023-06-30 14:51:18 -0400

    built out the big blocks for the infra

--- infrastructure/lib/incident-cdk-stack.ts
+++ infrastructure/lib/incident-cdk-stack.ts
@@ -18,121 +18,15 @@ export class IncidentCdkStack extends cdk.Stack {
   }
 
   private async initializeAsync(projectConfig: ProjectConfig) {
-    const buckets = new BucketConstruct(this, "BucketConstruct");
-
     const tables = new TableConstruct(this, "TableConstruct");
 
-    const authLambda = await lambdas.buildAuthLambda(this, { projectConfig });
-
-    const searchGifLambda = await lambdas.buildSearchGifLambda(this, {
+    const getIncidentLambda = await lambdas.buildGetIncidentLambda(this, {
       projectConfig,
-    });
-
-    const textToAudioLambda = await lambdas.buildTextToAudioLambda(this, {
-      projectConfig,
-      audioBucket: buckets.audioBucket,
-      projectSectionTable: tables.projectSectionTable,
-    });
-
-    const getTranscriptLambda = await lambdas.buildGetTranscriptLambda(this, {
-      projectConfig,
-      projectSectionTable: tables.projectSectionTable,
-    });
-
-    const getGifLambda = await lambdas.buildGetGifLambda(this, {
-      projectConfig,
-      gifBucket: buckets.gifBucket,
-      projectSectionTable: tables.projectSectionTable,
-    });
-
-    const updateSectionLambda = await lambdas.buildUpdateSectionLambda(this, {
-      projectConfig,
-      projectSectionTable: tables.projectSectionTable,
-    });
-
-    const updateProjectLambda = await lambdas.buildUpdateProjectLambda(this, {
-      projectConfig,
-      projectTable: tables.projectTable,
-    });
-
-    const fargateConstruct = new FargateConstruct(this, "FargateConstruct", {
-      audioBucket: buckets.audioBucket,
-      gifBucket: buckets.gifBucket,
-      videoBucket: buckets.videoBucket,
-      projectSectionTable: tables.projectSectionTable,
-    });
-
-    const createResultLambda = await lambdas.buildCreateResultLambda(this, {
-      projectConfig,
-      resultTable: tables.projectResultTable,
-    });
-
-    const getResultLambda = await lambdas.buildGetResultLambda(this, {
-      projectConfig,
-      videoBucket: buckets.videoBucket,
-      resultTable: tables.projectResultTable,
-    });
-
-    const stateMachine = new StepFunctionConstruct(
-      this,
-      "StepFunctionConstruct",
-      {
-        getGifLambda,
-        getTranscriptLambda,
-        textToAudioLambda,
-        updateProjectLambda,
-        updateSectionLambda,
-        createResultLambda,
-        fargateConstruct,
-      }
-    );
-
-    // NOTE: avoids circular dependency
-    stateMachine.finalizeProjectStateMachine.grantTaskResponse(
-      fargateConstruct.taskDefinition.taskRole
-    );
-
-    const startProjectLambda = await lambdas.buildStartProjectLambda(this, {
-      projectConfig,
-      projectTable: tables.projectTable,
-      startProjectStateMachine: stateMachine.startProjectStateMachine,
-    });
-
-    const finalizeProjectLambda = await lambdas.buildFinalizeProjectEventLambda(
-      this,
-      {
-        projectConfig,
-        projectTable: tables.projectTable,
-        projectSectionTable: tables.projectSectionTable,
-        finalizeProjectStateMachine: stateMachine.finalizeProjectStateMachine,
-      }
-    );
-
-    const getProjectsLambda = await lambdas.buildGetProjectsLambda(this, {
-      projectConfig,
-      projectTable: tables.projectTable,
-    });
-
-    const getProjectLambda = await lambdas.buildGetProjectLambda(this, {
-      projectConfig,
-      projectTable: tables.projectTable,
-      sectionTable: tables.projectSectionTable,
-    });
-
-    const editSectionLambda = await lambdas.buildEditSectionLambda(this, {
-      projectConfig,
-      sectionTable: tables.projectSectionTable,
+      incidentTable: tables.incidentTable,
     });
 
     new ApiConstruct(this, "ApiConstruct", {
-      authLambda,
-      startProjectLambda,
-      finalizeProjectLambda,
-      searchGifLambda,
-      getProjectsLambda,
-      getProjectLambda,
-      getResultLambda,
-      editSectionLambda,
+      getIncidentLambda,
     });
   }
 }
