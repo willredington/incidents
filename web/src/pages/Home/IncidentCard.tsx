b3cb439d8b99f6bcb4c529114014d916fe36a02d
Will <will86325@gmail.com>
2023-06-30 16:36:54 -0400

    added incident card header

--- web/src/pages/Home/IncidentCard.tsx
+++ web/src/pages/Home/IncidentCard.tsx
@@ -1,10 +1,13 @@
 import {
+  Box,
   Card,
   CardBody,
   CardHeader,
   HStack,
   Heading,
+  Tag,
   Text,
+  VStack,
 } from "@chakra-ui/react";
 import { IncidentData } from "../../models/incident";
 import { formatDateTime } from "../../utils/date";
@@ -13,12 +16,31 @@ export function IncidentCard({ incident }: { incident: IncidentData }) {
   return (
     <Card size={"lg"}>
       <CardHeader>
-        <HStack justify={"space-between"}>
-          <Text fontWeight={"semibold"} fontSize={"2xl"}>
-            Incident
-          </Text>
-          <Text>#{incident.description.incident_number}</Text>
-        </HStack>
+        <VStack spacing={4} alignItems={"stretch"}>
+          <HStack justify={"space-between"} alignItems={"baseline"}>
+            <HStack alignItems={"baseline"}>
+              <Text fontWeight={"semibold"} fontSize={"2xl"}>
+                Incident
+              </Text>
+              <Text>#{incident.description.incident_number}</Text>
+            </HStack>
+            <Tag alignSelf={"flex-start"}>{incident.description.type}</Tag>
+          </HStack>
+          <HStack justify={"space-between"}>
+            <VStack align={"flex-start"}>
+              <Text fontWeight={"semibold"}>Opened</Text>
+              <Text fontSize={"sm"} fontStyle={"italic"}>
+                {formatDateTime(incident.description.event_opened)}
+              </Text>
+            </VStack>
+            <VStack align={"flex-start"}>
+              <Text fontWeight={"semibold"}>Closed</Text>
+              <Text fontSize={"sm"} fontStyle={"italic"}>
+                {formatDateTime(incident.description.event_closed)}
+              </Text>
+            </VStack>
+          </HStack>
+        </VStack>
       </CardHeader>
       <CardBody>
         <p>incident data goes here</p>
--- web/src/utils/date.ts
+++ web/src/utils/date.ts
@@ -1,5 +1,5 @@
 import { DateTime } from "luxon";
 
 export function formatDateTime(isoDateString: string) {
-  return DateTime.fromISO(isoDateString).toFormat("yyyy-MM-dd HH:mm:ss");
+  return DateTime.fromISO(isoDateString).toFormat("yyyy-MM-dd hh:mm:ss a");
 }
